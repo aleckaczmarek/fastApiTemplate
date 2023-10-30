7f56b8f7f6bd2ca5b901a29b48c27b964224995a
aleckaczmarek <aleckaczmarek@gmail.com>
2023-10-30 15:58:23 -0700

    generic controller

--- src/model/User.py
+++ src/model/User.py
@@ -15,17 +15,11 @@ class User(BaseModel):
     last_name: Optional[str] = None
     address_id: Optional[int] = None
     email_address: Optional[str] = None
-    collection_name:str = "Users" 
+    collection_name:str = "users" 
 
     def __init__(self,Id=None, first_name=None, last_name=None, address_id=None, email_address=None,collection_name="Users"):
         print("user init")
         super().__init__(Id=Id, first_name=first_name, last_name=last_name, address_id=address_id, email_address=email_address,collection_name=collection_name)
-    
-    # def __init__(self,data):
-    #     super().__init__(Id=data.Id, first_name=data.first_name, last_name=data.last_name, address_id=data.address_id, email_address=data.email_address)
-
-    # def __init__(self):
-    #     super().__init__(Id=None, first_name=None, last_name=None, address_id=None, email_address=None)
 
     def build(self,key,value):
         setattr(self,key,value)
