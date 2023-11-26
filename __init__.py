1af717cd8c8a5cee7ef5214ed3c8494e24a08835
aleckaczmarek <aleckaczmarek@gmail.com>
2023-11-26 13:56:07 -0800

    init py restruct

--- __init__.py
+++ __init__.py
@@ -1,30 +1,54 @@
 macroMapper/
   __init__.py
-  src/main.py
-  src/system/auth/
-    Security.py
-  src/api/feature/
-    user/
-      UserAPI.py
-      UserUtils.py
-    contract/
-      ContractAPI.py
-      ContractUtils.py
-  src/api/model/
-    User.py
-    Contract.py
-  src/system/repository/
-    Repository.py
-  src/system/service/
-    Service.py
-  src/system/transporters/
-    Data.py
-    Result.py
-  src/system/util/
-    DBConnect.py
-    General.py
-    Middleware.py
-    HttpUtils.py
-    Routes.py
+  src/
+    main.py
+    system/
+      auth/
+        Security.py
+      service/
+        Service.py
+      repository/
+        Repository.py
+      transporters/
+        Data.py
+        Result.py
+      util/
+        DBConnect.py
+        General.py
+        Middleware.py
+        HttpUtils.py
+        Routes.py
+    api/
+      feature/
+        user/
+          UserAPI.py
+          UserUtils.py
+        contract/
+          ContractAPI.py
+          ContractUtils.py
+      model/
+        User.py
+        Contract.py
+
+  # src/system/auth/
+  #   Security.py
+  # src/api/feature/
+    
+  # src/api/model/
+  #   User.py
+  #   Contract.py
+  # src/system/repository/
+  #   Repository.py
+  # src/system/service/
+  #   Service.py
+  # src/system/transporters/
+  #   Data.py
+  #   Result.py
+  # src/system/util/
+  #   DBConnect.py
+  #   General.py
+  #   Middleware.py
+  #   HttpUtils.py
+  #   Routes.py
   tests/
     test_repository.py
