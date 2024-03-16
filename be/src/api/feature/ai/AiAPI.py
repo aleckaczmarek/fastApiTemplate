9499fc7c2c33deb234f34f445f9fbf67d5941d44
aleckaczmarek <aleckaczmarek@gmail.com>
2024-03-16 14:11:11 -0700

    set llama2 as base /ask algorithm added back ask/{ollama_llm_name}

--- be/src/api/feature/ai/AiAPI.py
+++ be/src/api/feature/ai/AiAPI.py
@@ -13,26 +13,21 @@ from system.auth.Security import validate_token
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=OAuth2PasswordBearer_Token_URL)
 router = APIRouter()
 auth_scheme = HTTPBearer()  
-# ask_ai(token:HTTPAuthorizationCredentials = Depends(auth_scheme))
-# Ready
-# @router.get('/api/ai/ask/{question}', response_model=Result)
-# async def ask_ai(question):
-#     # await validate_token(token.credentials,["admin"],["admin:all"])
-#     ai = AiConnect("orca-mini")  
-#     chat = await ai.getResponse(question)
-#     return Result().build("data",chat)
 
 @router.post('/api/ai/ask',response_model=Result)
 async def ask_ai_post(data:AiModel):
     question = data.question
     print("INSIDE POST REQ ", data)
-    ai = AiConnect("codellama")  
+    ai = AiConnect("llama2")  
     chat = await ai.getResponse(question)
     return Result().build("data",chat)
 
-# @router.get('/api/ai/ask/{llm}/{question}', response_model=Result)
-# async def ask_ai(llm, question):
-#     # await validate_token(token.credentials,["admin"],["admin:all"])
-#     ai = AiConnect(llm)  
-#     chat = await ai.getResponse(question)
-#     return Result().build("data",chat)
+
+@router.post('/api/ai/ask/{ollama_llm_name}',response_model=Result)
+async def ask_ai_post(data:AiModel,ollama_llm_name):
+    question = data.question
+    print("INSIDE POST REQ ", data)
+    ai = AiConnect(ollama_llm_name)  
+    chat = await ai.getResponse(question)
+    return Result().build("data",chat)
+ 
